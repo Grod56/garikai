f04689f35e7a43f166ce04c55b7eabe1488c7440
Grod56 <rodneygaryx29@gmail.com>
2025-02-10 22:43:12 +0200

    Risky commits (corrections)

--- app/components/ImageCard/ImageCardSkeletonModel.ts
+++ app/components/ImageCard/ImageCardSkeletonModel.ts
@@ -5,6 +5,10 @@ export class ImageCardSkeletonModel implements Model {
     constructor() {
 
     }
-    
+    private _id: any;
+
+    public get id(): any {
+        return this._id;
+    }
     nameOfClass: string = _NAME_OF_CLASS;
 }
