96c259698c506dc17647b0f380a07ee7c65f31ae
Grod56 <rodneygaryx29@gmail.com>
2025-05-13 15:03:19 +0200

    General refactoring

--- app-library/utilities/miscelleneous.ts
+++ app-library/utilities/miscelleneous.ts
@@ -1,5 +1,3 @@
-import { Model } from "../custom-types/model/Model";
-
 export function compositeClassNameResolver(...classNames: string[]): string {
 	return classNames.join(" ");
 }
@@ -8,5 +6,3 @@ export function removeMarkup(markedUpText: string): string {
 	const regex: RegExp = /(<([^>]+)>)/gi;
 	return markedUpText.replace(regex, "");
 }
-
-export type DeepInferModel<M extends Model<U>, U extends object> = T & Model<U>;
