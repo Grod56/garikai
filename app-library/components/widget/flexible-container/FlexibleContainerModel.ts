46d557453740b78da6e426a5dd0dc7f27bc3543f
Grod56 <rodneygaryx29@gmail.com>
2025-05-21 23:04:59 +0200

    Migrated mvc framework to new repo

--- app-library/components/widget/flexible-container/FlexibleContainerModel.ts
+++
@@ -1,6 +0,0 @@
-import { ReadonlyModel } from "@mvc-react/mvc";
-import { WidgetComponentModelView } from "../WidgetComponentModel";
-
-export type FlexibleContainerModel = ReadonlyModel<FlexibleContainerModelView>;
-
-export type FlexibleContainerModelView = WidgetComponentModelView;
