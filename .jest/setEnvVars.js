ade63e7affbe0b891d76ab270eecae0b1659d5ed
Grod56 <rodneygaryx29@gmail.com>
2025-06-01 12:31:13 +0200

    divested from bootstrap

---
+++ .jest/setEnvVars.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+
+process.env.NEXT_PUBLIC_SUPABASE_URL = "https://random765fjhhgvjg.supabase.co";
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY =
+	"blahblahblah67543254637855utyghbhjv hgvcxrexetgfhg";
+process.env.NEXT_PUBLIC_BLOGGER_POSTS_ENDPOINT_URL =
+	"https://www.googleapis.com/blogger/v3/blogs/5876753424901103466/posts";
