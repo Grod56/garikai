768f39d45dac86dfaca9c698959bf8de00f8ccff
Grod56 <rodneygaryx29@gmail.com>
2025-06-05 17:34:47 +0200

    migrated to Neon

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-undef */
 
+process.env.DATABASE_URL =
+	"postgres://neondb_owner:blahblahblahasldmasldmalsmiefjoiwefn@ep-tdsfsdfsdfrfsddee.us-east-1.aws.neon.tech/neondb?sslmode=require";
+process.env.NEXT_PUBLIC_SUPABASE_URL = "https://random765fjhhgvjg.supabase.co";
 process.env.NEXT_PUBLIC_SUPABASE_URL = "https://random765fjhhgvjg.supabase.co";
 process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY =
 	"blahblahblah67543254637855utyghbhjv hgvcxrexetgfhg";
