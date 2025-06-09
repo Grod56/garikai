8f15e6e5c3cce6918f5d72dab9ca1b76a8d537c4
Grod56 <rodneygaryx29@gmail.com>
2025-06-09 04:30:32 +0200

    removed blurs for performance

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
 	title: "Home",
 	description:
 		"Welcome! You've found my digital home. This place is the nexus of all of my interests, hobbies, and projects.",
+	alternates: {
+		canonical: "./",
+	},
 };
 
 export default function Page() {
